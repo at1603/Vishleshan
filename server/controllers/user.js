833023090decf0be9563777428c4099ff45daf9e
Harsh Pandey <harshpandey011@gmail.com>
2021-07-30 21:41:07 +0530

    added user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,1 +1,21 @@
-//user.js
+import Users from "../models/user.js"
+
+export const signup = async (req, res) => {
+    const data = req.body
+    const newUser = new Users(data)
+    try {
+        await newUser.save()
+        res.status(201).json(newUser)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export const signin = async (req, res) => {
+
+    try {
+        console.log("trial")
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
