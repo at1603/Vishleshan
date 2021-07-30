eeaffc47ad0034b0e4a10ba0e70f85193670979a
Harsh Pandey <harshpandey011@gmail.com>
2021-07-30 21:51:38 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 import express from 'express';
-import bodyParser from  'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+import authRoutes from './routes/users.js'
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 
+app.use('/', authRoutes)
+
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${process.env.PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
