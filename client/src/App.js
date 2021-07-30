4c5d12fd040caa347601a2e805aa8ca297db8bd3
Harsh Pandey <harshpandey011@gmail.com>
2021-07-30 22:52:32 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import { Container, AppBar, Typography, Grow, makeStyles, IconButton, Toolbar } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 
+import Home from './components/Home/Home'
 import Auth from './components/Auth/Auth';
 
 
@@ -21,23 +24,24 @@ const App = () => {
 
     const classes = useStyles()
     return (
-        <Container maxWidth='lg'>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        News
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Grow in >
-                <Container>
-                    <Auth />
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxWidth='lg'>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            News
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/auth" exact component={Auth} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
     )
 }
 
