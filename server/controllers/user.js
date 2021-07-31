b2da776ff686c39984031872e6978303cbbc61d9
Harsh Pandey <harshpandey011@gmail.com>
2021-07-31 18:10:38 +0530

    added authentication

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,21 +1,46 @@
-import Users from "../models/user.js"
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import User from "../models/user.js"
 
 export const signup = async (req, res) => {
-    const data = req.body
-    const newUser = new Users(data)
+    console.log(req.body)
+    const { email, password, confirmPassword, firstName, lastName } = req.body;
+
     try {
-        await newUser.save()
-        res.status(201).json(newUser)
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            res.status(400).json({ message: "User already exist!" });
+        }
+
+        if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match! " });
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+        const token = jwt.sign({ email: result.email, id: result._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        res.status(200).json({ result, token });
     } catch (error) {
-        res.status(404).json({ message: error.message })
+        res.status(500).json({ message: "Something went wrong!" })
     }
+
 }
 
 export const signin = async (req, res) => {
+    const { email, password } = req.body;
 
     try {
-        console.log("trial")
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser) {
+            res.status(404).json({ message: "User doesn't exist!" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid Credentials." });
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        res.status(200).json({ result: existingUser, token });
     } catch (error) {
-        res.status(404).json({ message: error.message })
+        res.status(500).json({ message: "Something went wrong!" })
     }
 }
