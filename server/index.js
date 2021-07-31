bc0ed8daed1b1de0661d14a5f01cfc1fd791d058
Pro-Abhinav <abhinav16032002@gmail.com>
2021-07-31 19:22:38 +0530

    Added authentication

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import authRoutes from './routes/users.js'
+import authRoutes from './routes/auth.js'
 
 dotenv.config();
 const app = express();
@@ -16,7 +16,6 @@ app.use(cors());
 
 
 const PORT = process.env.PORT || 5000
-
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
