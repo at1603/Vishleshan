d31cdd1e4130ebd3e1d3adcd3771735200e62c73
Pro-Abhinav <abhinav16032002@gmail.com>
2021-07-31 21:36:27 +0530

    Added Home Page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const Home = () => {
+import { Button, Typography, Paper, Grid} from '@material-ui/core';
+import useStyles from './styles';
+
+const Home = () => {   
+    const classes = useStyles();
+
     return (
-        <h1>Hello</h1>
+        <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+                <Grid container justifyContent="center" spacing={6}>
+                    <Grid item>
+                        <Paper className={classes.paper}>
+                            <Button component={Link} to="/dashboard" variant="h3" align="center">Dashboard</Button>
+                        </Paper>
+                    </Grid>
+                    <Grid item>
+                        <Paper className={classes.paper}>
+                            <Button component={Link} to="/pitchAnalysis" variant="h3" align="center">Pitch Analysis</Button>
+                        </Paper>
+                    </Grid>
+                    <Grid item>
+                        <Paper className={classes.paper} textAlign="center">
+                            <Button component={Link} to="/interviewAnalysis" variant="h3">Interview Analysis  </Button>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Grid>
+      </Grid>
     )
 }
 
