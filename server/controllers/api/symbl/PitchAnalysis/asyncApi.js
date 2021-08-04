e39e9fc33f79ff301e75c4983453d67962886216
Pro-Abhinav <abhinav16032002@gmail.com>
2021-08-04 20:24:22 +0530

    added pitch analysis backend;

---
+++ server/controllers/api/symbl/PitchAnalysis/asyncApi.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+export const getVideoData = (req, res) => {
+    console.log(req.body);
+}
--- server/routes/analysis.js
+++ server/routes/analysis.js
@@ -1,15 +1,17 @@
 import express from 'express';
 
 import { startInterviewAnalysis, InterviewAnalysisResult, stopInterviewAnalysis } from '../controllers/api/symbl/InterviewAnalysis/zoomApi.js'
+import { getVideoData } from '../controllers/api/symbl/PitchAnalysis/asyncApi.js'
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 
-
-
-// router.post('/stopInterviewAnalysis', auth, stopInterviewAnalysis)
+router.post('/stopInterviewAnalysis', auth, stopInterviewAnalysis)
 router.post('/interviewAnalysisResult', auth, InterviewAnalysisResult)
 router.post('/interviewAnalysis', auth, startInterviewAnalysis);
+
+//------------Pitch Analysis routes------------//
+router.post('/pitchAnalysis/getVideoData', auth, getVideoData);
 
 export default router;
