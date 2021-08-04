e4bc4361ea4ad59d5040c8b5447bf5f1f9624478
Anmol Sahu <anmolsahu2k@gmail.com>
2021-08-04 23:34:02 +0530

    minor changes

--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -1,19 +1,19 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      marginTop: 250
-    },
-    paper: {
-        display: 'flex',
-        height: 300,
-        width: 400,
-        justifyContent: 'center'
-    },
-    control: {
-      padding: theme.spacing(2),
-    },
-  }));
+  root: {
+    flexGrow: 1,
+    marginTop: 250
+  },
+  paper: {
+    display: 'flex',
+    height: 300,
+    width: 400,
+    justifyContent: 'center'
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
-  export default useStyles;
+export default useStyles;
