e4249350ead1e342bef8332f2a883762c3934adf
Pro-Abhinav <abhinav16032002@gmail.com>
2021-08-04 20:24:03 +0530

    added pitch analysis page

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -4,3 +4,5 @@ export const LOGOUT = 'LOGOUT'
 export const JOIN_CALL = 'JOIN_CALL'
 export const GET_INTERVIEW_ANALYSIS = 'GET_INTERVIEW_ANALYSIS'
 export const STOP_ANALYSIS = 'STOP_ANALYSIS'
+
+export const SEND_VIDEO_DATA = 'SEND_VIDEO_DATA'
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import interviewAnalysis from './interviewAnalysis';
+import pitchAnalysis from './pitchAnalysis';
 
 export default combineReducers({
     auth,
-    interviewAnalysis
+    interviewAnalysis,
+    pitchAnalysis
 });
