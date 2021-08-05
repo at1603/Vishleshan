a7c9d37bdd3769666a52db5600d9be905b38846b
Anmol Sahu <anmolsahu2k@gmail.com>
2021-08-05 15:50:23 +0530

    Improve Auth UX

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { Button, Typography, Paper, Grid } from '@material-ui/core'
+import { Button, Typography, Paper, Grid, FormControlLabel, Checkbox, Link, Container, CssBaseline, Box } from '@material-ui/core'
 import { GoogleLogin } from 'react-google-login';
 import { signin, signup } from '../../actions/auth';
 import useStyles from './styles';
@@ -9,6 +9,21 @@ import useStyles from './styles';
 import Input from './Input';
 
 import Icon from './Icon';
+
+
+const Copyright = () => {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Vishleshan
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
 
 const Auth = () => {
     const [formData, setFormData] = useState({
@@ -63,43 +78,56 @@ const Auth = () => {
     };
 
     return (
-        <Paper>
-            <form onSubmit={handleSubmit}>
-                <Typography>Create An Account!</Typography>
-                <Grid container spacing={2}>
-                    {isSignup && (
-                        <>
-                            <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
-                            <Input name="lastName" label="Last Name" handleChange={handleChange} half />
-                        </>
-                    )}
-
-                    <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
-                    <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
-                    {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
-                </Grid>
-                <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
                     {isSignup ? 'Sign Up' : 'Sign In'}
-                </Button>
-                <GoogleLogin
-                    clientId="474688141554-p30r33nv95t7r6me30nr1ttl01u74hs6.apps.googleusercontent.com"
-                    render={(renderProps) => (
-                        <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
-                            Google Sign In
-                        </Button>
-                    )}
-                    onSuccess={googleSuccess}
-                    onFailure={googleFailure}
-                    cookiePolicy="single_host_origin"
-                />
+                </Typography>
+                <form onSubmit={handleSubmit} className={classes.form}>
+                    <Grid container spacing={2}>
+                        {isSignup && (
+                            <>
+                                <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                                <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+                            </>
+                        )}
 
-                <Grid container justifyContent="flex-end">
-                    <Grid item>
-                        <Button onClick={switchMode}>{isSignup ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}</Button>
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+                        {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
                     </Grid>
-                </Grid>
-            </form>
-        </Paper>
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        {isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <GoogleLogin
+                        clientId="474688141554-p30r33nv95t7r6me30nr1ttl01u74hs6.apps.googleusercontent.com"
+                        render={(renderProps) => (
+                            <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
+                                Google Sign In
+                            </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleFailure}
+                        cookiePolicy="single_host_origin"
+                    />
+
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="#" variant="body2" onClick={switchMode}>{isSignup ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}</Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={5}>
+                <Copyright />
+            </Box>
+        </Container>
+
     )
 }
 
