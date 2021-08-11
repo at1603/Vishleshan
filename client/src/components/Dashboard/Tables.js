7dab7537359a7d6b5fbe1a2a9826d66415bdc6aa
Anmol Sahu <anmolsahu2k@gmail.com>
2021-08-11 18:19:42 +0530

    Add Table to Dashboard

---
+++ client/src/components/Dashboard/Tables.js
@@ -0,0 +1,74 @@
+import { React, useEffect } from 'react';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+// Generate Order Data
+function createData(id, date, name, shipTo, paymentMethod, amount) {
+    return { id, date, name, shipTo, paymentMethod, amount };
+}
+
+const rows = [
+    createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
+    createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
+    createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
+    createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
+    createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
+];
+
+function preventDefault(event) {
+    event.preventDefault();
+}
+
+const useStyles = makeStyles((theme) => ({
+    seeMore: {
+        marginTop: theme.spacing(3),
+    },
+}));
+
+export default function Tables() {
+    const analysisData = JSON.parse(localStorage.getItem('pitchAnalysisData'))
+    const classes = useStyles();
+
+    // useEffect(() => {
+
+    //     dispatch(getvideodata(conversationId1,conversationId2))
+
+    //     });
+    // }, [])
+    return (
+        <React.Fragment>
+            <Table size="medium">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Sr No.</TableCell>
+                        <TableCell>Meeting Name</TableCell>
+                        <TableCell>Created At</TableCell>
+                        <TableCell>Conversation Id</TableCell>
+                        <TableCell align="right">Comparison</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.id}>
+                            <TableCell>{row.date}</TableCell>
+                            <TableCell>{row.name}</TableCell>
+                            <TableCell>{row.shipTo}</TableCell>
+                            <TableCell>{analysisData.messages.messages[0].conversationId}</TableCell>
+                            <TableCell align="right">{row.amount}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <div className={classes.seeMore}>
+                <Link color="primary" href="#" onClick={preventDefault}>
+                    See more orders
+                </Link>
+            </div>
+        </React.Fragment>
+    );
+}
