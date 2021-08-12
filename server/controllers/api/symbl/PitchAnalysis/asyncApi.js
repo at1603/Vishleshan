86ed88b41714446d2fd217bf30814a1356f4b1ff
Harsh Pandey <harshpandey011@gmail.com>
2021-08-12 21:05:56 +0530

    added meeting titleand restructured response

--- server/controllers/api/symbl/PitchAnalysis/asyncApi.js
+++ server/controllers/api/symbl/PitchAnalysis/asyncApi.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import { getAbuseAnalysis, getEmotionAnalysis, getIntentAnalysis, getSarcasmAnalysis } from '../../Komprehend/komprehend.js';
 import AnalysisData from '../../../../models/analysisDataModel.js';
 
-const startPitchAnalysis = (authToken, path, callback) => {
+const startPitchAnalysis = (authToken, path, meetingName, callback) => {
     let data = {}
     const check = async (jobId, conversationId) => {
         request.get({
@@ -71,11 +71,10 @@ const startPitchAnalysis = (authToken, path, callback) => {
 
 
 
-
-    const videoFileStream = fs.createReadStream('public/zoom_0.mp4');
+    const videoFileStream = fs.createReadStream(path);
 
     const params = {
-        'name': "Business Meeting",
+        'name': meetingName,
         // <Optional, string| your_conversation_name | Your meeting name. Default name set to conversationId.>
 
         'confidenceThreshold': 0.5,
@@ -164,7 +163,6 @@ export const sendVideoData = (req, res) => {
             }
         })
         var upload = multer({ storage: storage }).single('file')
-
         upload(req, res, function (err) {
             if (err instanceof multer.MulterError) {
                 return res.status(500).json(err)
@@ -172,10 +170,11 @@ export const sendVideoData = (req, res) => {
                 return res.status(500).json(err)
             }
             console.log("ss")
-            const path = 'public/zoom_0.mp4'
+            const path = `public/${req.body.fileName}`
             generateAuthToken((authToken) => {
-                console.log(authToken, "bbb")
-                startPitchAnalysis(authToken.accessToken, path, async (err, data) => {
+                console.log(req.body, "jjjjjjjjjjjjjj")
+
+                startPitchAnalysis(authToken.accessToken, path, req.body.meetingName, async (err, data) => {
                     if (err) {
                         console.log(err)
                     }
@@ -185,21 +184,46 @@ export const sendVideoData = (req, res) => {
                             const existingUser = await AnalysisData.findOne({ handlerId: req.userId })
                             if (!existingUser) {
                                 try {
-                                    await AnalysisData.create({ handlerId: req.userId, conversationIdData: { conversationId: data.messages.messages[0].conversationId, createdAt: Date.now() }, analysisData: data });
-                                    mongoose.connection.close()
-                                    res.status(200).json(data)
+                                    await AnalysisData.create({ handlerId: req.userId, conversationIdData: { conversationId: data.messages.messages[0].conversationId, createdAt: Date.now(), meetingName: req.body.meetingName }, analysisData: data });
+                                    // mongoose.connection.close()
+                                    const finalData = data.messages.messages
+                                    let i;
+                                    for (i = 0; i < finalData.length; i++) {
+                                        let tempEmotion = analysisData.extraAnalysis.emotion[i].emotion
+                                        let tempIntent = analysisData.extraAnalysis.intent[i].intent
+                                        let tempProfane = analysisData.extraAnalysis.profaneWord[i]
+                                        let tempSarcasm = analysisData.extraAnalysis.sarcasm[i]
+                                        finalData[i].emotion = Object.keys(tempEmotion).reduce((a, b) => tempEmotion[a] > tempEmotion[b] ? a : b)
+                                        finalData[i].intent = Object.keys(tempIntent).reduce((a, b) => tempIntent[a] > tempIntent[b] ? a : b)
+                                        finalData[i].profane = Object.keys(tempProfane).reduce((a, b) => tempProfane[a] > tempProfane[b] ? a : b)
+                                        finalData[i].sarcasm = Object.keys(tempSarcasm).reduce((a, b) => tempSarcasm[a] > tempSarcasm[b] ? a : b)
+                                    }
+                                    res.status(200).json(finalData)
                                 }
                                 catch (error) {
                                     console.log(error.message)
                                 }
                             }
                             else {
-                                await AnalysisData.updateOne({ _id: existingUser._id }, { $push: { conversationIdData: { conversationId: data.messages.messages[0].conversationId, createdAt: Date.now() }, analysisData: data } }).exec(function (err, response) {
+                                await AnalysisData.updateOne({ _id: existingUser._id }, { $push: { conversationIdData: { conversationId: data.messages.messages[0].conversationId, createdAt: Date.now(), meetingName: req.body.meetingName }, analysisData: data } }).exec(function (err, response) {
                                     if (err) {
                                         console.log(err)
                                     }
                                     else {
-                                        res.status(200).json(data)
+                                        const finalData = data.messages.messages
+                                        let i;
+                                        for (i = 0; i < finalData.length; i++) {
+                                            let tempEmotion = analysisData.extraAnalysis.emotion[i].emotion
+                                            let tempIntent = analysisData.extraAnalysis.intent[i].intent
+                                            let tempProfane = analysisData.extraAnalysis.profaneWord[i]
+                                            let tempSarcasm = analysisData.extraAnalysis.sarcasm[i]
+                                            finalData[i].emotion = Object.keys(tempEmotion).reduce((a, b) => tempEmotion[a] > tempEmotion[b] ? a : b)
+                                            finalData[i].intent = Object.keys(tempIntent).reduce((a, b) => tempIntent[a] > tempIntent[b] ? a : b)
+                                            finalData[i].profane = Object.keys(tempProfane).reduce((a, b) => tempProfane[a] > tempProfane[b] ? a : b)
+                                            finalData[i].sarcasm = Object.keys(tempSarcasm).reduce((a, b) => tempSarcasm[a] > tempSarcasm[b] ? a : b)
+                                        }
+                                        // mongoose.connection.close()
+                                        res.status(200).json(finalData)
                                     }
                                 })
                             }
