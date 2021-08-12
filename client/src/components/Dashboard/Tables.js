9ee603aaee91f68f1ed02f208e09719e3858fcad
Anmol Sahu <anmolsahu2k@gmail.com>
2021-08-12 15:46:46 +0530

    Add Table UI

--- client/src/components/Dashboard/Tables.js
+++ client/src/components/Dashboard/Tables.js
@@ -1,4 +1,5 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -6,69 +7,450 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { useHistory } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import { getconversationlist } from '../../actions/pitchAnalysis';
+import { getvideodata } from '../../actions/pitchAnalysis';
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { lighten } from "@material-ui/core/styles";
+import TableContainer from "@material-ui/core/TableContainer";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import DeleteIcon from "@material-ui/icons/Delete";
+import FilterListIcon from "@material-ui/icons/FilterList";
 
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-    return { id, date, name, shipTo, paymentMethod, amount };
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-    createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
-    createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
-    createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-    createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
-    createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Donut", 452, 25.0, 51, 4.9),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("Honeycomb", 408, 3.2, 87, 6.5),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Jelly Bean", 375, 0.0, 94, 0.0),
+    createData("KitKat", 518, 26.0, 65, 7.0),
+    createData("Lollipop", 392, 0.2, 98, 0.0),
+    createData("Marshmallow", 318, 0, 81, 2.0),
+    createData("Nougat", 360, 19.0, 9, 37.0),
+    createData("Oreo", 437, 18.0, 63, 4.0)
 ];
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === "desc"
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+
+    { id: "protein", numeric: true, disablePadding: false, label: "Sr. No." },
+    {
+        id: "name",
+        numeric: false,
+        disablePadding: true,
+        label: "Meeting Name"
+    },
+    { id: "calories", numeric: true, disablePadding: false, label: "Created At" },
+    { id: "fat", numeric: true, disablePadding: false, label: "Conversation ID" },
+    { id: "carbs", numeric: true, disablePadding: false, label: "Carbs (g)" },
+];
+
+function EnhancedTableHead(props) {
+    const {
+        classes,
+        onSelectAllClick,
+        order,
+        orderBy,
+        numSelected,
+        rowCount,
+        onRequestSort
+    } = props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={"center"}
+                        padding={headCell.disablePadding ? "none" : "normal"}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : "asc"}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <span className={classes.visuallyHidden}>
+                                    {order === "desc" ? "sorted descending" : "sorted ascending"}
+                                </span>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+                {/* <TableCell padding="checkbox">
+                    <Checkbox
+                        indeterminate={numSelected > 0 && numSelected < rowCount}
+                        checked={rowCount > 0 && numSelected === rowCount}
+                        onChange={onSelectAllClick}
+                        inputProps={{ "aria-label": "select all meetings" }}
+                    />
+                </TableCell> */}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired
+};
+
+const useToolbarStyles = makeStyles((theme) => ({
+    root: {
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(1)
+    },
+    highlight:
+        theme.palette.type === "light"
+            ? {
+                color: theme.palette.secondary.main,
+                backgroundColor: lighten(theme.palette.secondary.light, 0.85)
+            }
+            : {
+                color: theme.palette.text.primary,
+                backgroundColor: theme.palette.secondary.dark
+            },
+    title: {
+        flex: "1 1 100%"
+    }
+}));
+
+const EnhancedTableToolbar = (props) => {
+    const classes = useToolbarStyles();
+    const { numSelected } = props;
+
+    return (
+        <Toolbar
+            className={clsx(classes.root, {
+                [classes.highlight]: numSelected > 0
+            })}
+        >
+            {numSelected > 0 ? (
+                <Typography
+                    className={classes.title}
+                    color="inherit"
+                    variant="subtitle1"
+                    component="div"
+                >
+                    {numSelected} Selected
+                </Typography>
+            ) : (
+                <Typography
+                    className={classes.title}
+                    variant="h6"
+                    id="tableTitle"
+                    component="div"
+                >
+                    Select 2 rows to compare
+                </Typography>
+            )}
+
+            {numSelected == 2 ? (
+                <Tooltip title="Delete">
+                    {/* <IconButton aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton> */}
+                    <Button>Compare</Button>
+                </Tooltip>
+            ) : (
+                <Tooltip title="Filter list">
+                    <IconButton aria-label="filter list">
+                        <FilterListIcon />
+                    </IconButton>
+                </Tooltip>
+            )}
+        </Toolbar>
+    );
+};
+
+EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired
+};
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: "100%"
+    },
+    paper: {
+        width: "100%",
+        marginBottom: theme.spacing(2)
+    },
+    table: {
+        minWidth: 750
+    },
+    visuallyHidden: {
+        border: 0,
+        clip: "rect(0 0 0 0)",
+        height: 1,
+        margin: -1,
+        overflow: "hidden",
+        padding: 0,
+        position: "absolute",
+        top: 20,
+        width: 1
+    }
+}));
+
+// export default function EnhancedTable() {
+
+// }
+
+
 
 function preventDefault(event) {
     event.preventDefault();
 }
 
-const useStyles = makeStyles((theme) => ({
-    seeMore: {
-        marginTop: theme.spacing(3),
-    },
-}));
+// const useStyles = makeStyles((theme) => ({
+//     seeMore: {
+//         marginTop: theme.spacing(3),
+//     },
+// }));
 
 export default function Tables() {
-    const analysisData = JSON.parse(localStorage.getItem('pitchAnalysisData'))
+    const dispatch = useDispatch();
+    const history = useHistory();
     const classes = useStyles();
 
-    // useEffect(() => {
+    const conversationId1 = "5024370436079616";
+    const conversationId2 = "5665967414706176";
 
-    //     dispatch(getvideodata(conversationId1,conversationId2))
+    useEffect(() => {
+        // dispatch(getvideodata(conversationId1, conversationId2, history));
+        dispatch(getconversationlist());
 
-    //     });
-    // }, [])
+    }, [dispatch]);
+
+    const conversationList = useSelector((state) => state);
+    const tableData = conversationList.pitchAnalysis.conversationIdData;
+    // console.log(tableData.map((items) => items.conversationId));
+    console.log(conversationList);
+
+    // const classes = useStyles();
+    const [order, setOrder] = useState("asc");
+    const [orderBy, setOrderBy] = useState("calories");
+    const [selected, setSelected] = useState([]);
+    const [page, setPage] = useState(0);
+    const [dense, setDense] = useState(false);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === "asc";
+        setOrder(isAsc ? "desc" : "asc");
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelecteds = rows.map((n) => n.name);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleClick = (event, name) => {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected = [];
+
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1)
+            );
+        }
+
+        setSelected(newSelected);
+        console.log(newSelected);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleChangeDense = (event) => {
+        setDense(event.target.checked);
+    };
+
+    const isSelected = (name) => selected.indexOf(name) !== -1;
+
+    const emptyRows =
+        rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
     return (
-        <React.Fragment>
-            <Table size="medium">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Sr No.</TableCell>
-                        <TableCell>Meeting Name</TableCell>
-                        <TableCell>Created At</TableCell>
-                        <TableCell>Conversation Id</TableCell>
-                        <TableCell align="right">Comparison</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.id}>
-                            <TableCell>{row.date}</TableCell>
-                            <TableCell>{row.name}</TableCell>
-                            <TableCell>{row.shipTo}</TableCell>
-                            <TableCell>{analysisData.messages.messages[0].conversationId}</TableCell>
-                            <TableCell align="right">{row.amount}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-            <div className={classes.seeMore}>
-                <Link color="primary" href="#" onClick={preventDefault}>
-                    See more orders
-                </Link>
-            </div>
-        </React.Fragment>
+        <div className={classes.root}>
+            <Paper className={classes.paper}>
+                <EnhancedTableToolbar numSelected={selected.length} />
+                <TableContainer>
+                    <Table
+                        className={classes.table}
+                        aria-labelledby="tableTitle"
+                        size="medium"
+                        aria-label="enhanced table"
+                    >
+                        <EnhancedTableHead
+                            classes={classes}
+                            numSelected={selected.length}
+                            order={order}
+                            orderBy={orderBy}
+                            // onSelectAllClick={handleSelectAllClick}
+                            onRequestSort={handleRequestSort}
+                            rowCount={rows.length}
+                        />
+                        <TableBody>
+                            {stableSort(rows, getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row, index) => {
+                                    const isItemSelected = isSelected(row.name);
+                                    const labelId = `enhanced-table-checkbox-${index}`;
+
+                                    return (
+                                        <TableRow
+                                            hover
+                                            // onClick={(event) => handleClick(event, row.name)}
+                                            // role="checkbox"
+                                            aria-checked={isItemSelected}
+                                            tabIndex={-1}
+                                            key={row.name}
+                                            selected={isItemSelected}
+                                        >
+                                            <TableCell
+                                                component="th"
+                                                id={labelId}
+                                                scope="row"
+                                                padding="none"
+                                                align="center"
+                                            >
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell align="center">{row.calories}</TableCell>
+                                            <TableCell align="center">{row.fat}</TableCell>
+                                            <TableCell align="center">{row.carbs}</TableCell>
+                                            <TableCell align="center">{row.protein}</TableCell>
+                                            <TableCell padding="checkbox">
+                                                <Checkbox
+                                                    checked={isItemSelected}
+                                                    inputProps={{ "aria-labelledby": labelId }}
+                                                    disabled={(selected.length == 2 && !isItemSelected) ? true : false}
+                                                    onClick={(event) => handleClick(event, row.name)}
+                                                />
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            {emptyRows > 0 && (
+                                <TableRow style={{ height: (53) * emptyRows }}>
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </div>
     );
+    // return (
+    //     <>
+    //         <Table size="medium">
+    //             <TableHead>
+    //                 <TableRow>
+    //                     <TableCell>Sr No.</TableCell>
+    //                     <TableCell>Meeting Name</TableCell>
+    //                     <TableCell>Created At</TableCell>
+    //                     <TableCell>Conversation Id</TableCell>
+    //                     <TableCell align="right">Comparison</TableCell>
+    //                 </TableRow>
+    //             </TableHead>
+    //             <TableBody>
+    //                 {tableData.map((row, index) => (
+    //                     <TableRow key={row._id}>
+    //                         <TableCell>{index + 1}</TableCell>
+    //                         <TableCell>{123}</TableCell>
+    //                         <TableCell>{row.createdAt}</TableCell>
+    //                         <TableCell>{row.conversationId}</TableCell>
+    //                         <TableCell align="right">{890}</TableCell>
+    //                     </TableRow>
+    //                 ))}
+    //             </TableBody>
+    //         </Table>
+    //         <div className={classes.seeMore}>
+    //             <Link color="primary" href="#" onClick={preventDefault}>
+    //                 See more orders
+    //             </Link>
+    //         </div>
+    //     </>
+    // );
 }
