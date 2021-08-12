dcd9c6bdad8346b108a217eb5a7cc4d4309c50e0
Harsh Pandey <harshpandey011@gmail.com>
2021-08-13 00:46:55 +0530

    restructured pitchAnalysis actions

--- client/src/actions/pitchAnalysis.js
+++ client/src/actions/pitchAnalysis.js
@@ -1,7 +1,6 @@
 import * as api from '../api';
 import { SEND_VIDEO_DATA } from '../constants/actionTypes';
 import { GET_VIDEO_DATA } from '../constants/actionTypes';
-import { GET_CONVO_LIST } from '../constants/actionTypes';
 
 export const sendvideodata = (formData, history) => async (dispatch) => {
     try {
@@ -16,23 +15,14 @@ export const sendvideodata = (formData, history) => async (dispatch) => {
 
 export const getvideodata = (conversationId1, conversationId2, history) => async (dispatch) => {
     try {
+        console.log(conversationId1, conversationId2, "Aaaaaa")
         const { data } = await api.getVideoData(conversationId1, conversationId2);
         dispatch({ type: GET_VIDEO_DATA, data })
-        history.push('/');
+        history.push('/home');
         console.log(data);
     } catch (error) {
         console.log(error);
     }
 }
 
-export const getconversationlist = () => async (dispatch) => {
-    try {
-        console.log("in getconvo");
-        const { data } = await api.getConversationList();
-        dispatch({ type: GET_CONVO_LIST, data });
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
-}
 
