c80d51de55844e52f2f82644d7bc3cad1e8f1d2e
Pro-Abhinav <abhinav16032002@gmail.com>
2021-08-14 22:27:36 +0530

    add localStorage data

--- client/src/reducers/dashboard.js
+++ client/src/reducers/dashboard.js
@@ -4,6 +4,7 @@ import { GET_CONVO_LIST } from '../constants/actionTypes';
 const dashboardReducer = (state = { comparisonData: {}, conversationList: {} }, action) => {
     switch (action.type) {
         case GET_COMPARISON_DATA:
+            localStorage.setItem('comparisonData', JSON.stringify({ ...action?.data }));
             return { ...state, comparisonData: action?.data };
         case GET_CONVO_LIST:
             return { ...state, conversationList: action?.data };
